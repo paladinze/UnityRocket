9468f8145e3029f599baa5bf7165df60f1ba3577
paladinze <paladinze@hotmail.com>
2019-01-03 13:27:19 +0800

    added mock script for rocket

---
+++ Assets/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput ()
+    {
+        // forward and backward
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thrust");
+        }
+
+        // rotating
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("Turn Right");
+        } else if (Input.GetKey(KeyCode.A))
+        {
+            print("Turn Left");
+        }
+    }
+}
